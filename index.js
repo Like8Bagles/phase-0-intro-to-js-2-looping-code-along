c78279a1b4821efd2e9936e678ea3cd9c092a718
Anthony <anthonykmbanks@gmail.com>
2021-03-07 14:36:08 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return newArray
+}
+
+
+function countDown(number = 10) {
+    while (number > -1) {
+        console.log(number--)
+    }
+}
